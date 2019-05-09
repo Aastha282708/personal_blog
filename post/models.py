cbc8f2dbec197ee7ac778e7e95c69bfdef6193a7
Aastha282708 <timalsinaastha01@gmail.com>
2019-05-09 07:21:08 +0545

    Codebase Initial Commit

--- post/models.py
+++ post/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextFiled()
+    created_at=models.DateTimeField(auto_now=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    author=models.
+
